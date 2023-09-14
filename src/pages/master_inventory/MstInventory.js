2119ae7c82a585f19e5618e14dd620e1d5e2389d
masato.ichikawa <masato.ichikawa.engineer@gmail.com>
2023-09-14 20:36:42 +0900

    wip

--- src/pages/master_inventory/MstInventory.js
+++ src/pages/master_inventory/MstInventory.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import Box from '@mui/material/Box';
 import { DataGrid, jaJP } from '@mui/x-data-grid';
 
 import MstInventoryButton from "./buttons/MstInventoryButton.js";
@@ -60,7 +61,7 @@ function MstInventory(){
       },
       {
         field: 'id',
-        headerName: 'ID'
+        headerName: 'ID',
       },
       {
         field: 'name',
@@ -124,25 +125,49 @@ function MstInventory(){
       {
         field: 'display',
         headerName: '表示'
-      }
-      ]
-
-    const text = jaJP.components.MuiDataGrid.defaultProps.localeText;
-
+      }];
+    cols.forEach((a) => {
+      a.headerAlign='center';
+      a.headerClassName='datagrid-header';
+      a.cellClassName='datagrid-cell';
+      a.disableColumnMenu=true;
+      a.sortable=false;
+    });
 
     return (
         <>
         <MstInventoryButton setReload={setReload} />
-        <section className='mstinventorygrid'>
-            <DataGrid 
+
+        <Box
+              sx={{
+                height: 'calc(100vh - 190px)',
+                width: '100%',
+                '& .datagrid-header': {
+                  borderBottom: 'solid 1px rgba(224, 224, 224, 1)',
+                },
+                '.datagrid-header, .datagrid-cell:not(:last-child)': {
+                  borderRight: 'solid 1px rgba(224, 224, 224, 1) !important',
+                  userSelect:'none',
+                },
+                '.MuiDataGrid-columnHeaders' : {
+                  backgroundColor: '#65b2c6', 
+                  color: '#fff',
+                }
+              }}>
+            <DataGrid className='mstinventorygrid'
             rows={rows} 
             columns={cols}
-            disableColumnFilter={false}
-            filterMode="server"
-            showColumnRightBorder={true}
-            localeText={text}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 10,
+                },
+              },
+            }}
+            pageSizeOptions={[10]}
+            localeText={jaJP.components.MuiDataGrid.defaultProps.localeText}
             />
-        </section>
+        </Box>
         </>
     );
 }
